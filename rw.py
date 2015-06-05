f6105c0ffc690fa8f50bcd56ff26b391b79214e3
Laurent Courty <lrntct@users.noreply.github.com>
2015-06-05 16:39:56 -0500

    change messenging to Messenger()

--- rw.py
+++ rw.py
@@ -4,12 +4,14 @@
 # COPYRIGHT:    (C) 2015 by Laurent Courty
 #
 #               This program is free software under the GNU General Public
-#               License (v3). Read the file LICENCE for details.
+#               License (v3). Read the LICENCE file for details.
 
 import numpy as np
 from grass.pygrass import raster, utils
-import grass.script as grass
+from grass.pygrass.messages import Messenger
 
+# start messenger
+msgr = Messenger(raise_on_error=True)
 
 def format_opt_map(opt, mapset):
     """ analyse grass command line option
@@ -28,7 +30,7 @@ def write_sim_data(opt_h, opt_wse, h_grid_np1, z_grid, can_ovr,
     """
 
     if opt_h or opt_wse:
-        grass.info(_("Printing results, simulation time: %.1f ") %
+        msgr.message(_("Printing results, simulation time: %.1f ") %
                  round(sim_clock,1))
         if opt_h:
             # define new map name
@@ -37,7 +39,7 @@ def write_sim_data(opt_h, opt_wse, h_grid_np1, z_grid, can_ovr,
             h_map = np.where(h_grid_np1 == 0, np.nan, h_grid_np1)
             # write raster
             write_raster(depth_map_name, h_map, can_ovr)
-            grass.info(_("Writting water depth map <%s> ") % depth_map_name)
+            msgr.message(_("Writting water depth map <%s> ") % depth_map_name)
             # add map name to the map list
             if depth_map_name not in list_h:
                 list_h.append(depth_map_name)
@@ -49,7 +51,7 @@ def write_sim_data(opt_h, opt_wse, h_grid_np1, z_grid, can_ovr,
             wse_map = np.where(h_grid_np1 == 0, np.nan, h_grid_np1 + z_grid)
             # write raster
             write_raster(wse_map_name, wse_map, can_ovr)
-            grass.info(_("Writting water surface elevation map <%s> ") % wse_map_name)
+            msgr.message(_("Writting water surface elevation map <%s> ") % wse_map_name)
             # add map name to the map list
             if wse_map_name not in list_wse:
                 list_wse.append(wse_map_name)
@@ -75,7 +77,7 @@ def write_raster(raster_name, arr, can_ovr):
     """
     if can_ovr == True and raster.RasterRow(raster_name).exist() == True:
         utils.remove(raster_name, 'raster')
-        grass.verbose(_("Removing raster map %s") % raster_name)
+        msgr.verbose(_("Removing raster map %s") % raster_name)
     with raster.RasterRow(raster_name, mode='w', mtype='DCELL') as newraster:
         newrow = raster.Buffer((arr.shape[1],), mtype='DCELL')
         for row in arr:
