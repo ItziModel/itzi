8d814cdea374194f772b953a18efe089e6866036
Laurent Courty <lrntct@gmail.com>
2016-02-04 17:26:29 -0600

    map_is_name(): avoid error message on output

--- gis.py
+++ gis.py
@@ -139,7 +139,7 @@ class Igis(object):
         False if not
         """
         try:
-            grass.read_command('r.info', map=map_id,flags='r')
+            grass.read_command('g.findfile', file='start_h@3x3_big', element='cell')
         except CalledModuleError:
             return False
         else:
