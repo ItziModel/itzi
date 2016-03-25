ItziModel/itzi

commit 0f31d1e6e51621354639c266f275a91cb2ddeac3

default infiltration to zero, without computation(Issue #36)

simulation.py
@@ -78,10 +78,14 @@ class SuperficialFlowSimulation(object):
         if self.in_map_names['in_inf']:
             self.inftype = 'fix'
             self.infiltration = infiltration.InfConstantRate()
-        else:
+        elif self.in_map_names['in_cap_pressure']:
             self.inftype = 'ga'
             self.infiltration = infiltration.InfGreenAmpt(self.gis.xr,
                                                           self.gis.yr)
+        else:
+            self.inftype = None
+        # instantiate an array with defult at zero
+        self.arr_inf = self.zeros_array()
 
         # dict to store TimedArrays objects
         self.tarrays = dict.fromkeys(self.in_map_names.keys())
@@ -291,15 +295,18 @@ class SuperficialFlowSimulation(object):
         if self.inftype == 'fix':
             self.infiltration.update_input(
                 self.tarrays['in_inf'].get_array(self.sim_time))
+            self.arr_inf[:] = self.infiltration.get_inf_rate(arr_h)
         elif self.inftype == 'ga':
             self.infiltration.update_input(
                 self.tarrays['in_eff_por'].get_array(self.sim_time),
                 self.tarrays['in_cap_pressure'].get_array(self.sim_time),
                 self.tarrays['in_hyd_conduct'].get_array(self.sim_time))
+            self.arr_inf[:] = self.infiltration.get_inf_rate(arr_h)
+        elif self.inftype is None:
+            pass  # arr_inf is set to zero at init
         else:
             assert False, "unknown infiltration type"
-        # set infiltration rate array
-        self.arr_inf = self.infiltration.get_inf_rate(arr_h)
+
 
     def update_domain_arrays(self, rast_dom):
         """Takes a SurfaceDomain object as input

commit de616ec14c8d8518b2df548dc8782c81b6fb5d2a

add an InfNull class to fix a bug introduced by 0f31d1e

infiltration.py
@@ -96,6 +96,10 @@ class InfGreenAmpt(Infiltration):
                     self.init_wat_soil_content, self.infrate, self.dt)
         return self.infrate
 
+class InfNull(Infiltration):
+    """Dummy class for cases where no inflitration is calculated
+    """
+    pass
 
 #~ class InfHoltan(Infiltration):
     #~ """

simulation.py
@@ -84,6 +84,7 @@ class SuperficialFlowSimulation(object):
                                                           self.gis.yr)
         else:
             self.inftype = None
+            self.infiltration = infiltration.InfNull()
         # instantiate an array with defult at zero
         self.arr_inf = self.zeros_array()
 