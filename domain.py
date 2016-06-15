re-organize general time-stepping and fix mass balance calculation

domain.py
@@ -14,6 +14,7 @@ GNU General Public License for more details.
 """
 from __future__ import division
 import math
+from datetime import timedelta
 import numpy as np
 try:
     import bottleneck as bn
@@ -22,7 +23,7 @@ except ImportError:
 
 import flow
 import time
-from itzi_error import NullError
+from itzi_error import NullError, DtError
 
 
 class SuperficialSimulation(object):
@@ -33,10 +34,8 @@ class SuperficialSimulation(object):
      - positive from West to East and from North to South
     """
 
-    def __init__(self, domain, param,
-                 sim_clock=0):
+    def __init__(self, domain, param):
         self.dom = domain
-        self.sim_clock = sim_clock
         self.dtmax = param['dtmax']
         self.cfl = param['cfl']
         self.g = param['g']
@@ -47,6 +46,8 @@ class SuperficialSimulation(object):
         self.dx = domain.dx
         self.dy = domain.dy
         self.cell_surf = self.dx * self.dy
+
+        self._dt = None
 
         # Slices for upstream and downstream cells on a padded array
         self.su = slice(None, -2)
@@ -96,11 +97,10 @@ class SuperficialSimulation(object):
         flow.flow_dir(arr_max_dz[self.s_i_0], dW, dE, self.dom.get('dire'))
         return self
 
-    def step(self, next_ts):
+    def step(self):
         """Run a full simulation time-step
         """
         start_time = time.time()
-        self.set_dt(next_ts)
         self.solve_q()
         self.apply_boundary_conditions()
         self.update_h()
@@ -113,7 +113,7 @@ class SuperficialSimulation(object):
         step_duration = end_time - start_time
         return self
 
-    def set_dt(self, next_ts):
+    def solve_dt(self):
         """Calculate the adaptative time-step
         The formula #15 in almeida et al (2012) has been modified to
         accomodate non-square cells
@@ -122,19 +122,31 @@ class SuperficialSimulation(object):
         maxh = self.dom.amax('h')  # max depth in domain
         min_dim = min(self.dx, self.dy)
         if maxh > 0:
-            self.dt = min(self.dtmax,
-                          self.cfl * (min_dim /
-                                      (math.sqrt(self.g * maxh))))
+            dt = self.cfl * (min_dim / (math.sqrt(self.g * maxh)))
+            self._dt = min(self.dtmax, dt)
         else:
-            self.dt = self.dtmax
-
-        # set sim_clock and check if timestep is within forced_timestep
-        if self.sim_clock + self.dt > next_ts:
-            self.dt = next_ts - self.sim_clock
-            self.sim_clock += self.dt
-        else:
-            self.sim_clock += self.dt
+            self._dt = self.dtmax
         return self
+
+    @property
+    def dt(self):
+        return timedelta(seconds=self._dt)
+
+    @dt.setter
+    def dt(self, newdt):
+        """return an error if new dt is higher than current one or negative
+        """
+        newdt_s = newdt.total_seconds()
+        fudge = timedelta.resolution.total_seconds()
+        if self._dt is None:
+            self._dt = newdt_s
+        elif newdt_s <= 0:
+            raise DtError(u"dt must be positive ({})".format(newdt_s))
+        elif newdt_s > self._dt + fudge:
+            raise DtError(u"new dt cannot be longer than current one "
+                          u"(old: {}, new: {})".format(self._dt, newdt_s))
+        else:
+            self._dt = newdt_s
 
     def domain_volume(self):
         '''return domain volume
@@ -156,7 +168,7 @@ class SuperficialSimulation(object):
                      arr_qn=flow_north, arr_qs=flow_south,
                      arr_bct=self.dom.get('bct'), arr_bcv=self.dom.get('bcv'),
                      arr_h=self.dom.get('h'), arr_hmax=self.dom.get('hmax'),
-                     dx=self.dx, dy=self.dy, dt=self.dt,
+                     dx=self.dx, dy=self.dy, dt=self._dt,
                      hfix_vol=self.dom.hfix_vol)
         assert not np.any(self.dom.get('h') < 0)
         return self
@@ -172,7 +184,7 @@ class SuperficialSimulation(object):
                      arr_hfe=self.dom.get('hfe'), arr_hfs=self.dom.get('hfs'),
                      arr_v=self.dom.get('v'), arr_vdir=self.dom.get('vdir'),
                      arr_vmax=self.dom.get('vmax'),
-                     dt=self.dt, dx=self.dx, dy=self.dy, g=self.g,
+                     dt=self._dt, dx=self.dx, dy=self.dy, g=self.g,
                      theta=self.theta, hf_min=self.hf_min,
                      v_rout=self.v_routing, sl_thres=self.sl_thresh)
         return self
@@ -241,25 +253,6 @@ class SuperficialSimulation(object):
         self.dom.swap_arrays('qe', 'qe_new')
         self.dom.swap_arrays('qs', 'qs_new')
         return self
-
-    def get_output_arrays(self, out_names):
-        """Takes a dict of map names
-        return a dict of unmasked arrays
-        """
-        out_arrays = {}
-        if out_names['out_h'] is not None:
-            out_arrays['out_h'] = self.dom.get_unmasked('h')
-        if out_names['out_wse']  is not None:
-            out_arrays['out_wse'] = self.dom.get_unmasked('h') + self.dom.get('z')
-        if out_names['out_v']  is not None:
-            out_arrays['out_v'] = self.dom.get_unmasked('v')
-        if out_names['out_vdir']  is not None:
-            out_arrays['out_vdir'] = self.dom.get_unmasked('vdir')
-        if out_names['out_qx']  is not None:
-            out_arrays['out_qx'] = self.dom.get_unmasked('qe_new') * self.dy
-        if out_names['out_qy']  is not None:
-            out_arrays['out_qy'] = self.dom.get_unmasked('qs_new') * self.dx
-        return out_arrays
 
 
 class Boundary(object):

rasterdomain.py
@@ -105,12 +105,10 @@ class RasterDomain(object):
         self.boundary_vol = 0.
         # volume passing through in-domain fixed depth boundaries
         self.hfix_vol = 0.
-        # volume of water in the domain
-        self.water_volume = 0.
         # flows sum
-        self.rain_q = 0.
-        self.inf_q = 0.
-        self.inflow_q = 0.
+        self._rain_q = 0.
+        self._inf_q = 0.
+        self._inflow_q = 0.
 
         # slice for a simple padding (allow stencil calculation on boundary)
         self.simple_pad = (slice(1,-1), slice(1,-1))
@@ -152,6 +150,29 @@ class RasterDomain(object):
 
         # Instantiate TimedArrays
         self.create_timed_arrays()
+
+    @property
+    def water_volume(self):
+        """get current water volume in the domain"""
+        return self.asum('h') * self.cell_surf
+
+    @property
+    def inf_q(self):
+        if self.isnew['inf']:
+            self._inf_q = self.asum('inf') / self.mmh_to_ms * self.cell_surf
+        return self._inf_q
+
+    @property
+    def rain_q(self):
+        if self.isnew['rain']:
+            self._rain_q = self.asum('rain') / self.mmh_to_ms * self.cell_surf
+        return self._rain_q
+
+    @property
+    def inflow_q(self):
+        if self.isnew['in_q']:
+            self._inflow_q = self.asum('in_q') * self.cell_surf
+        return self._inflow_q
 
     def zeros_array(self):
         """return a np array of the domain dimension, filled with zeros
@@ -203,7 +224,7 @@ class RasterDomain(object):
         mask = np.logical_or(np.isnan(arr), self.mask)
         arr[mask] = default_value
         assert not np.any(np.isnan(arr))
-        return arr
+        return self
 
     def unmask_array(self, arr):
         '''Replace values in the input array by NULL values from mask
@@ -213,40 +234,33 @@ class RasterDomain(object):
         return unmasked_array
 
     def update_input_arrays(self, sim_time):
-        """get new array using TimedArray
+        """Get new array using TimedArray
+        First update the DEM and mask if needed
         Replace the NULL values (mask)
         """
+        # make sure DEM is treated first
+        if not self.tarr['z'].is_valid(sim_time):
+            self.arr['z'][:] = self.tarr['z'].get(sim_time)
+            self.isnew['z'] = True
+            # note: must run update_flow_dir() in SuperficialSimulation
+            self.update_mask(self.arr['z'])
+            self.mask_array(self.arr['z'], np.finfo(self.dtype).max)
+
+        # loop through the other arrays
         for k, ta in self.tarr.iteritems():
             if not ta.is_valid(sim_time):
+                if k == 'z':
+                    continue  # z is done before
                 self.arr[k][:] = ta.get(sim_time)
                 self.isnew[k] = True
-                if k == 'z':
-                    self.update_mask(self.arr[k])
-                    default_value = np.finfo(self.dtype).max
-                    # must run update_flow_dir() in SuperficialSimulation
-                elif k == 'n':
+                if k == 'n':
                     default_value = 1
                 else:
                     default_value = 0
                 # mask arrays
-                self.arr[k][:] = self.mask_array(self.arr[k], default_value)
-                # calculate statistics
-                if k == 'h':
-                    self.water_volume = self.cell_surf * self.masked_sum('h')
-                elif k == 'rain':
-                    self.rain_q = self.masked_sum('rain') / self.mmh_to_ms * self.cell_surf
-                    print self.get('rain')
-                    print self.rain_q
-                elif k == 'in_inf':
-                    self.inf_q = self.masked_sum('in_inf') / self.mmh_to_ms * self.cell_surf
-                elif k == 'in_q':
-                    self.inflow_q = self.masked_sum('in_q') * self.cell_surf
-                else:
-                    pass
+                self.mask_array(self.arr[k], default_value)
             else:
                 self.isnew[k] = False
-        # update ext array
-        self.update_ext_array()
         return self
 
     def update_ext_array(self):
@@ -263,6 +277,24 @@ class RasterDomain(object):
         else:
             self.isnew['ext'] = False
         return self
+
+    def get_output_arrays(self):
+        """Returns a dict of unmasked arrays
+        """
+        out_arrays = {}
+        if self.out_map_names['out_h'] is not None:
+            out_arrays['out_h'] = self.get_unmasked('h')
+        if self.out_map_names['out_wse']  is not None:
+            out_arrays['out_wse'] = self.get_unmasked('h') + self.get('z')
+        if self.out_map_names['out_v']  is not None:
+            out_arrays['out_v'] = self.get_unmasked('v')
+        if self.out_map_names['out_vdir']  is not None:
+            out_arrays['out_vdir'] = self.get_unmasked('vdir')
+        if self.out_map_names['out_qx']  is not None:
+            out_arrays['out_qx'] = self.get_unmasked('qe_new') * self.dy
+        if self.out_map_names['out_qy']  is not None:
+            out_arrays['out_qy'] = self.get_unmasked('qs_new') * self.dx
+        return out_arrays
 
     def swap_arrays(self, k1, k2):
         """swap values of two arrays
@@ -291,8 +323,8 @@ class RasterDomain(object):
         """
         return np.amax(self.arr[k])
 
-    def masked_sum(self, k):
-        """return the sum of an unpadded array, only for the area of the mask
+    def asum(self, k):
+        """return the sum of an unpadded array
+        values outside the proper domain are the defaults values
         """
-        #~ return bn.nansum(self.arr[k][np.logical_not(self.mask)])
-        return bn.nansum(self.unmask_array(self.arr[k]))
+        return np.sum(self.arr[k])

simulation.py
@@ -36,6 +36,7 @@ class SimulationManager(object):
 
     def __init__(self, record_step, input_maps, output_maps,
                  dtype=np.float32,
+                 dtmin=timedelta(seconds=0.01),
                  stats_file=None,
                  start_time=datetime(1, 1, 1),
                  end_time=datetime(1, 1, 1),
@@ -53,19 +54,20 @@ class SimulationManager(object):
 
         self.record_step = record_step
         self.start_time = start_time
-        self.set_duration(end_time, sim_duration)
+        self.__set_duration(end_time, sim_duration)
         # set simulation time to start_time a the beginning
         self.sim_time = self.start_time
-        # dt
-        self.dt = sim_param['dtmax']
+        # First time-step is forced
+        self.dt = dtmin  # Global time-step
         self.dtinf = sim_param['dtinf']
         # set temporal type of results
-        self.set_temporal_type()
+        self.__set_temporal_type()
 
         # dictionaries of map names
         self.in_map_names = input_maps
         self.out_map_names = output_maps
 
+        # data type of arrays
         self.dtype = dtype
         # simulation parameters
         self.sim_param = sim_param
@@ -82,12 +84,9 @@ class SimulationManager(object):
         self.gis.read(self.in_map_names)
 
         # instantiate simulation objects
-        self.set_models()
+        self.__set_models()
 
-        # a dict containing lists of maps written to gis to be registered
-        self.output_maplist = {k: [] for k in self.out_map_names.keys()}
-
-    def set_duration(self, end_time, sim_duration):
+    def __set_duration(self, end_time, sim_duration):
         """If sim_duration is given, end_time is ignored
         This is normally checked upstream
         """
@@ -99,7 +98,7 @@ class SimulationManager(object):
             self.end_time = self.start_time + sim_duration
         return self
 
-    def set_temporal_type(self):
+    def __set_temporal_type(self):
         """A start_time equal to datetime.min means user did not
         provide a start_time. Therefore a relative temporal type
         is set for results writing.
@@ -112,7 +111,7 @@ class SimulationManager(object):
             self.temporal_type = 'relative'
         return self
 
-    def set_models(self):
+    def __set_models(self):
         """Instantiate models objects
         """
         # RasterDomain
@@ -121,11 +120,14 @@ class SimulationManager(object):
 
         # Infiltration. Coherence of input maps is checked upstream
         if self.in_map_names['in_inf']:
-            self.infiltration = infiltration.InfConstantRate(self.rast_domain)
+            self.infiltration = infiltration.InfConstantRate(self.rast_domain,
+                                                             self.dtinf)
         elif self.in_map_names['in_cap_pressure']:
-            self.infiltration = infiltration.InfGreenAmpt(self.rast_domain)
+            self.infiltration = infiltration.InfGreenAmpt(self.rast_domain,
+                                                          self.dtinf)
         else:
-            self.infiltration = infiltration.InfNull(self.rast_domain)
+            self.infiltration = infiltration.InfNull(self.rast_domain,
+                                                     self.dtinf)
 
         # SuperficialSimulation
         self.surf_sim = domain.SuperficialSimulation(self.rast_domain,
@@ -136,165 +138,88 @@ class SimulationManager(object):
             self.massbal = MassBal(self.stats_file, self.rast_domain)
         else:
             self.massbal = None
+        # reporting object
+        self.report = Report(self.gis, self.temporal_type, self.sim_param['hmin'],
+                             self.massbal, self.rast_domain)
         return self
 
     def run(self):
         """Perform a full simulation
-        including infiltration, superficial flow and drainage,
+        including infiltration, superficial flow etc.,
         recording of data and mass_balance calculation
         """
-        record_counter = 1
-        last_inf = 0.
         duration_s = self.duration.total_seconds()
+
+        # dict of next time-step (datetime object)
+        self.next_ts = {'end': self.end_time,
+                        'rec': self.start_time + self.record_step}
+        for k in ['inf', 'surf']:
+            self.next_ts[k] = self.start_time
+        # First time-step is forced
+        self.nextstep = self.sim_time + self.dt
 
         self.gis.msgr.verbose(_(u"Starting time-stepping..."))
         while self.sim_time < self.end_time:
+            self.sim_time_s = (self.sim_time - self.start_time).total_seconds()
             # display advance of simulation
-            self.gis.msgr.percent(self.surf_sim.sim_clock, duration_s, 1)
-
-            # Calculate when will happen the next records writing
-            next_record = record_counter * self.record_step.total_seconds()
-
-            # update arrays
+            self.gis.msgr.percent(self.sim_time_s, duration_s, 1)
+            # update input arrays
             self.rast_domain.update_input_arrays(self.sim_time)
             # recalculate the flow direction if DEM changed
             if self.rast_domain.isnew['z']:
                 self.surf_sim.update_flow_dir()
-
-            # calculate infiltration
-            self.set_inf_forced_timestep(next_record)
-            self.infiltration.set_dt(self.dtinf, self.surf_sim.sim_clock,
-                                     self.inf_forced_ts)
-            if last_inf + self.infiltration.dt >= self.surf_sim.sim_clock:
-                self.infiltration.step()
-                last_inf = float(self.surf_sim.sim_clock)
-                self.rast_domain.isnew['inf'] = True
-            else:
-                self.rast_domain.isnew['inf'] = False
-
-            # Update external arrays
-            self.rast_domain.update_ext_array()
-
-            # next forced flow time-step
-            next_ts = self.next_forced_timestep()
-            # step() raise NullError in case of NaN/NULL cell
-            # if this happen, stop simulation and
-            # output a map showing the errors
-            try:
-                self.surf_sim.step(next_ts)
-            except NullError:
-                self.write_error_to_gis(self.surf_sim.arr_err)
-                self.gis.msgr.fatal(_(u"Null value detected "
-                                      u"in simulation at time {}, "
-                                      u"terminating").format(self.sim_time))
-
-            # update simulation time and dt
-            self.sim_time = (self.start_time +
-                             timedelta(seconds=self.surf_sim.sim_clock))
-            self.dt = self.surf_sim.dt
-            if self.massbal:
-                self.massbal.read_values(self.dt)
-            # write simulation results
-            rec_time = self.surf_sim.sim_clock / self.record_step.total_seconds()
-            if rec_time >= record_counter:
-                self.gis.msgr.verbose(_(u"Writting output map..."))
-                self.output_arrays = self.surf_sim.get_output_arrays(self.out_map_names)
-                self.write_results_to_gis(record_counter)
-                record_counter += 1
-                if self.massbal:
-                    self.write_mass_balance(self.surf_sim.sim_clock)
-        # register generated maps in GIS
-        self.register_results_in_gis()
-        if self.out_map_names['out_h']:
-            self.write_hmax_to_gis()
-        if self.out_map_names['out_v']:
-            self.write_vmax_to_gis()
+            # step models
+            self.step()
+            # update simulation time
+            self.sim_time += self.dt
+        # write final report
+        self.report.end(self.sim_time)
         return self
 
-    def write_mass_balance(self, sim_clock):
-        '''
-        '''
-        if self.temporal_type == 'absolute':
-            self.massbal.write_values(self.sim_time)
-        elif self.temporal_type == 'relative':
-            self.massbal.write_values(sim_clock)
+    def step(self):
+        """Step each of the model if needed
+        """
+        # calculate infiltration
+        if self.sim_time == self.next_ts['inf']:
+            self.infiltration.solve_dt()
+            # calculate when will happen the next time-step
+            self.next_ts['inf'] += self.infiltration.dt
+            self.infiltration.step()
+            self.rast_domain.isnew['inf'] = True
         else:
-            assert False, "unknown temporal type!"
-        return self
+            self.rast_domain.isnew['inf'] = False
 
-    def next_forced_timestep(self):
-        """return the future time in seconds at which the superficial flow
-        model will be forced to step.
-        default to next forced time-step of infiltration model
-        """
-        return self.inf_forced_ts
+        # calculate superficial flow #
+        # update arrays of infiltration, rainfall etc.
+        self.rast_domain.update_ext_array()
+        # force time-step to be the general time-step
+        self.surf_sim.dt = self.dt
+        # step() raise NullError in case of NaN/NULL cell
+        # if this happen, stop simulation and
+        # output a map showing the errors
+        try:
+            self.surf_sim.step()
+        except NullError:
+            self.report.write_error_to_gis(self.surf_sim.arr_err)
+            self.gis.msgr.fatal(_(u"{}: "
+                                  u"Null value detected in simulation, "
+                                  u"terminating").format(self.sim_time))
+        # calculate when should happen the next surface time-step
+        self.surf_sim.solve_dt()
+        self.next_ts['surf'] += self.surf_sim.dt
 
-    def set_inf_forced_timestep(self, next_record):
-        """Given a next superficial flow time-step in seconds as entry,
-        return the future time in seconds at which the infiltration
-        model will be forced to step.
-        """
-        self.inf_forced_ts = min(next_record, self.duration.total_seconds())
-        return self
+        if self.massbal:
+            self.massbal.read_values(self.dt.total_seconds())
+        # write simulation results
+        if self.sim_time >= self.next_ts['rec']:
+            self.gis.msgr.verbose(_(u"{}: Writting output maps...".format(self.sim_time)))
+            self.report.step(self.sim_time)
+            self.next_ts['rec'] += self.record_step
 
-    def write_results_to_gis(self, record_counter):
-        """Format the name of each maps using the record number as suffix
-        Send a couple array, name to the GIS writing function.
-        """
-        for k, arr in self.output_arrays.iteritems():
-            if isinstance(arr, np.ndarray):
-                suffix = str(record_counter).zfill(4)
-                map_name = "{}_{}".format(self.out_map_names[k], suffix)
-                # Export depth if above hfmin. If not, export NaN
-                if k == 'out_h':
-                    with warnings.catch_warnings():
-                        warnings.simplefilter("ignore")
-                        arr[arr <= self.sim_param['hmin']] = np.nan
-                # write the raster
-                self.gis.write_raster_map(arr, map_name, k)
-                # add map name and time to the corresponding list
-                self.output_maplist[k].append((map_name, self.sim_time))
-        return self
-
-    def write_error_to_gis(self, arr_error):
-        '''Write a given depth array to the GIS
-        '''
-        map_h_name = "{}_error".format(self.out_map_names['out_h'])
-        self.gis.write_raster_map(self.rast_domain.get_unmasked('h'),
-                                  map_h_name, 'out_h')
-        # add map name to the revelant list
-        self.output_maplist['out_h'].append(map_h_name)
-        return self
-
-    def write_hmax_to_gis(self):
-        '''Write a given depth array to the GIS
-        '''
-        arr_hmax_unmasked = self.rast_domain.get_unmasked('hmax')
-        map_hmax_name = "{}_max".format(self.out_map_names['out_h'])
-        self.gis.write_raster_map(arr_hmax_unmasked, map_hmax_name, 'out_h')
-        return self
-
-    def write_vmax_to_gis(self):
-        '''Write a given flow speed array to the GIS
-        '''
-        arr_vmax_unmasked = self.rast_domain.get_unmasked('vmax')
-        map_vmax_name = "{}_max".format(self.out_map_names['out_v'])
-        self.gis.write_raster_map(arr_vmax_unmasked, map_vmax_name, 'out_v')
-        return self
-
-    def register_results_in_gis(self):
-        """Register the generated maps in the temporal database
-        Loop through output names
-        if no output name is provided, don't do anything
-        if name is populated, create a strds of the right temporal type
-        and register the corresponding listed maps
-        """
-        for mkey, lst in self.output_maplist.iteritems():
-            strds_name = self.out_map_names[mkey]
-            if strds_name is None:
-                continue
-            self.gis.register_maps_in_strds(mkey, strds_name, lst,
-                                            self.temporal_type)
+        # find next step
+        self.nextstep = min(self.next_ts.values())
+        self.next_ts['surf'] = self.nextstep
+        self.dt = self.nextstep - self.sim_time
         return self
 
 
@@ -303,8 +228,6 @@ class MassBal(object):
     Mass balance error is the difference between the actual volume and
     the theoretical volume. The later is the old volume + input - output.
     Intended use:
-    at each time-step, using add_value():
-    individual simulation operations send relevant values to the MassBal object
     at each record time, using write_values():
     averaged or cumulated values for the considered time difference are
     written to a CSV file
@@ -438,4 +361,114 @@ class MassBal(object):
         # empty dictionaries
         self.sim_data = {k: [] for k in self.sim_data.keys()}
         self.line = dict.fromkeys(self.line.keys())
+        return self
+
+
+class Report(object):
+    """In charge of results reporting and writing
+    """
+    def __init__(self, igis, temporal_type, hmin, massbal, rast_dom):
+        self.record_counter = 0
+        self.gis = igis
+        self.temporal_type = temporal_type
+        self.out_map_names = rast_dom.out_map_names
+        self.hmin = hmin
+        self.rast_dom = rast_dom
+        self.massbal = massbal
+        # a dict containing lists of maps written to gis to be registered
+        self.output_maplist = {k: [] for k in self.out_map_names.keys()}
+
+    def step(self, sim_time):
+        """write results at given time-step
+        """
+        assert isinstance(sim_time, datetime)
+        self.output_arrays = self.rast_dom.get_output_arrays()
+        self.write_results_to_gis(sim_time)
+        if self.massbal:
+            if self.temporal_type == 'relative':
+                sim_time = sim_time - datetime.min
+            self.write_mass_balance(sim_time)
+        self.record_counter += 1
+        return self
+
+    def end(self, sim_time):
+        """write last mass balance
+        register maps in gis
+        write max level maps
+        """
+        if self.massbal:
+            if self.temporal_type == 'relative':
+                sim_time = sim_time - datetime.min
+            self.write_mass_balance(sim_time)
+        self.register_results_in_gis()
+        if self.out_map_names['out_h']:
+            self.write_hmax_to_gis()
+        if self.out_map_names['out_v']:
+            self.write_vmax_to_gis()
+        return self
+
+    def write_mass_balance(self, sim_time):
+        """Append mass balance values to file
+        """
+        self.massbal.write_values(sim_time)
+        return self
+
+    def write_results_to_gis(self, sim_time):
+        """Format the name of each maps using the record number as suffix
+        Send a couple array, name to the GIS writing function.
+        """
+        for k, arr in self.output_arrays.iteritems():
+            if isinstance(arr, np.ndarray):
+                suffix = str(self.record_counter).zfill(4)
+                map_name = "{}_{}".format(self.out_map_names[k], suffix)
+                # Export depth if above hmin. If not, export NaN
+                if k == 'out_h':
+                    with warnings.catch_warnings():
+                        warnings.simplefilter("ignore")
+                        arr[arr <= self.hmin] = np.nan
+                # write the raster
+                self.gis.write_raster_map(arr, map_name, k)
+                # add map name and time to the corresponding list
+                self.output_maplist[k].append((map_name, sim_time))
+        return self
+
+    def write_error_to_gis(self, arr_error):
+        '''Write a given depth array to the GIS
+        '''
+        map_h_name = "{}_error".format(self.out_map_names['out_h'])
+        self.gis.write_raster_map(self.rast_dom.get_unmasked('h'),
+                                  map_h_name, 'out_h')
+        # add map name to the revelant list
+        self.output_maplist['out_h'].append(map_h_name)
+        return self
+
+    def write_hmax_to_gis(self):
+        '''Write a max depth array to the GIS
+        '''
+        arr_hmax_unmasked = self.rast_dom.get_unmasked('hmax')
+        map_hmax_name = "{}_max".format(self.out_map_names['out_h'])
+        self.gis.write_raster_map(arr_hmax_unmasked, map_hmax_name, 'out_h')
+        return self
+
+    def write_vmax_to_gis(self):
+        '''Write a max flow speed array to the GIS
+        '''
+        arr_vmax_unmasked = self.rast_dom.get_unmasked('vmax')
+        map_vmax_name = "{}_max".format(self.out_map_names['out_v'])
+        self.gis.write_raster_map(arr_vmax_unmasked, map_vmax_name, 'out_v')
+        return self
+
+    def register_results_in_gis(self):
+        """Register the generated maps in the temporal database
+        Loop through output names
+        if no output name is provided, don't do anything
+        if name is populated, create a strds of the right temporal type
+        and register the corresponding listed maps
+        """
+        for mkey, lst in self.output_maplist.iteritems():
+            strds_name = self.out_map_names[mkey]
+            if strds_name is None:
+                continue
+            self.gis.register_maps_in_strds(mkey, strds_name, lst,
+                                            self.temporal_type)
         return self