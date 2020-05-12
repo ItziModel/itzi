710c230b4cd963d69dac4af7a2426472052e0446
Laurent Courty <lrntct@gmail.com>
2020-05-11 19:16:05 -0500

    remove six as a requirement (no support for Python 2)

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ CLASSIFIERS = ["Development Status :: 4 - Beta",
 DESCR = "A 2D flood model using GRASS GIS as a back-end"
 
 
-REQUIRES = ['pyinstrument', 'networkx == 1.11', 'six', 'grass-session']
+REQUIRES = ['pyinstrument', 'networkx == 1.11', 'grass-session']
 
 
 # Set arguments according to compiler
