fix complete_domain_data computation

src/itzi/providers/icechunk_input.py
@@ -67,10 +67,30 @@ class IcechunkRasterInputProvider(RasterInputProvider):
         repo = icechunk.Repository.open(config["icechunk_storage"])
         session = repo.readonly_session(config["icechunk_group"])
         self.dataset = xr.open_zarr(session.store)
+        try:
+            self.crs_wkt = self.dataset.attrs["crs_wkt"]
+        except KeyError:
+            self.crs_wkt = ""
 
         if not self.is_dataset_sorted():
             raise ValueError("Coordinates must be sorted")
+        if not self.is_equal_spacing():
+            raise ValueError("Spatial coordinates must be equally spaced")
         self.temporal_type = self.detect_temporal_type()
+        self.origin = self.get_origin()
+
+    def is_equal_spacing(self) -> bool:
+        """
+        Check if spatial coordinates are equally spaced.
+        """
+        is_equal = []
+        for dim_name in [self.x_dim, self.y_dim]:
+            coord = self.dataset[dim_name]
+            diffs = np.diff(coord.values if hasattr(coord, "values") else coord)
+            if len(diffs) == 0:
+                return True, None
+            is_equal.append(np.allclose(diffs, diffs[0]))
+        return all(is_equal)
 
     def detect_temporal_type(self) -> str:
         """Detect if time coordinates are relative (timedelta) or absolute (datetime)."""
@@ -87,17 +107,34 @@ class IcechunkRasterInputProvider(RasterInputProvider):
             # No time dimension
             return TemporalType.ABSOLUTE
 
-    @property
-    def origin(self) -> Tuple[float, float]:
-        """Return the coordinates of the NW corner
-        as a tuple (N, W)"""
-        north = self.dataset[self.y_dim].values.max()
-        west = self.dataset[self.x_dim].values.min()
-        return north, west
-
     def get_domain_data(self) -> DomainData:
         """Return a DomainData object."""
-        pass
+        # Coordinates are at the center of the cells
+        y_coords = self.dataset[self.y_dim]
+        rows = len(y_coords)
+        northernmost_cell = y_coords.values.max()
+        southernmost_cell = y_coords.values.min()
+        nsres = (northernmost_cell - southernmost_cell) / (rows - 1)
+        north = northernmost_cell + nsres / 2
+        south = northernmost_cell - nsres / 2
+
+        x_coords = self.dataset[self.x_dim]
+        cols = len(x_coords)
+        easternmost_cell = x_coords.values.max()
+        westernmost_cell = x_coords.values.min()
+        ewres = (easternmost_cell - westernmost_cell) / (cols - 1)
+        east = easternmost_cell + ewres / 2
+        west = westernmost_cell - ewres / 2
+
+        return DomainData(
+            north=north,
+            south=south,
+            east=east,
+            west=west,
+            rows=rows,
+            cols=cols,
+            crs_wkt=self.crs_wkt,
+        )
 
     def is_dataset_sorted(self) -> bool:
         """Check if all coordinates are sorted"""

tests/test_icechunk_input.py
@@ -26,9 +26,22 @@ def input_maps_dict():
 def coordinates(input_maps_dict: Dict):
     """Generate x and y coordinates for the test arrays"""
     arr_shape = next(iter(input_maps_dict.values())).shape
-    y_coords = np.linspace(start=1000, stop=1000 + arr_shape[0] * 10, num=arr_shape[0])
-    x_coords = np.linspace(start=2000, stop=2000 + arr_shape[1] * 10, num=arr_shape[1])
-    return {"x_coords": x_coords, "y_coords": y_coords}
+    res = 10
+    y_start = 1000
+    y_stop = y_start + arr_shape[0] * res
+    x_start = 2000
+    x_stop = x_start + arr_shape[1] * res
+    y_coords = np.linspace(start=y_start, stop=y_stop, num=arr_shape[0], endpoint=False)
+    x_coords = np.linspace(start=x_start, stop=x_stop, num=arr_shape[1], endpoint=False)
+    print(f"{x_start=}, {x_stop=} {y_start=}, {y_stop}")
+    return {
+        "x_start": x_start,
+        "x_stop": x_stop,
+        "y_start": y_start,
+        "y_stop": y_stop,
+        "x_coords": x_coords,
+        "y_coords": y_coords,
+    }
 
 
 @pytest.fixture(scope="module")
@@ -379,8 +392,8 @@ def test_icechunk_input_provider_origin_property(
     assert isinstance(west, (int, float))
 
     # Expected NW corner: highest Y (North) and lowest X (West)
-    expected_north = coordinates["y_coords"].max()  # Highest Y coordinate
-    expected_west = coordinates["x_coords"].min()  # Lowest X coordinate
+    expected_north = coordinates["y_coords"].max() + 5  # Highest Y coordinate + half of resolution
+    expected_west = coordinates["x_coords"].min() - 5  # Lowest X coordinate
 
     assert north == expected_north
     assert west == expected_west