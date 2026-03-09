49320d856a8cd5dc24cd7f66989d085589aab3fa
Laurent Courty <lrntct@gmail.com>
2026-03-09 17:00:36 -0600

    update copyright notices to 2026

--- src/itzi/hotstart.py
+++ src/itzi/hotstart.py
@@ -1,5 +1,5 @@
 """
-Copyright (C) 2025 Laurent Courty
+Copyright (C) 2026 Laurent Courty
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
--- src/itzi/itzi_error.py
+++ src/itzi/itzi_error.py
@@ -1,5 +1,5 @@
 """
-Copyright (C) 2015-2017  Laurent Courty
+Copyright (C) 2015-2026 Laurent Courty
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
